b7cea2b72d0b56c5c90dd542218ee52abfc1eb10
Rajesh Durai <rajesh.durai@rea-group.com>
2020-08-14 23:13:31 +1000

    Create the Spree::Property if it doesn't exist.

--- app/services/solidus_import_products/save_properties.rb
+++ app/services/solidus_import_products/save_properties.rb
@@ -12,9 +12,25 @@ module SolidusImportProducts
       self.product = args[:product]
 
       properties_hash.each do |field, value|
-        property = Spree::Property.where('lower(name) = ?', field).first
+        normalized_property_name = field.to_s.downcase
+        property = Spree::Property.where(name: normalized_property_name).first
+
+        unless property
+          logger.log(
+            "INFO: Creating new Spree::Property: #{normalized_property_name}",
+            :info
+          )
+
+          property = Spree::Property.create(
+            name: normalized_property_name,
+            presentation: field.to_s.capitalize
+          )
+        end
+
         next unless property
+
         product_property = Spree::ProductProperty.where(product_id: product.id, property_id: property.id).first_or_initialize
+        logger.log("INFO: Existing ProductProperty: #{product_property}", :info)
         product_property.value = value
         product_property.save!
       end
